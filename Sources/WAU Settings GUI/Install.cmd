2700afe9da93eb21cdc64fb72b6c3e46bf0ac02a
KnifMelti <gaj@knifmelti.se>
2025-07-03 06:19:09 +0200

    Self-destructive 'Install.cmd' and better README

--- Sources/WAU Settings GUI/Install.cmd
+++ Sources/WAU Settings GUI/Install.cmd
@@ -1,1 +1,2 @@
 powershell -WindowStyle Hidden -Command "Start-Process PowerShell -WindowStyle Hidden -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0WAU-Settings-GUI.ps1\"' -Verb RunAs"
+del "%~f0"
