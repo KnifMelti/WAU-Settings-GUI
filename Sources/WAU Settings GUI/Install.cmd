a2174c23ec562057a8d72e07d42215216c9c7e0b
KnifMelti <gaj@knifmelti.se>
2025-07-09 22:37:42 +0200

    Remove Install.cmd script as part of cleanup

--- Sources/WAU Settings GUI/Install.cmd
+++
@@ -1,2 +0,0 @@
-powershell -WindowStyle Hidden -Command "Start-Process PowerShell -WindowStyle Hidden -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0WAU-Settings-GUI.ps1\"' -Verb RunAs"
-del "%~f0"
