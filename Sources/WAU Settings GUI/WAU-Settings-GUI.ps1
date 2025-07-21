1dc409dae40fde5da9a4239a49248f5f85c37d90
KnifMelti <gaj@knifmelti.se>
2025-07-21 19:48:51 +0200

    Bump to 1.8.1.3, only MsgBox in AHK if fromPS and small Release notes change

--- Sources/WAU Settings GUI/WAU-Settings-GUI.ps1
+++ Sources/WAU Settings GUI/WAU-Settings-GUI.ps1
@@ -2429,7 +2429,11 @@ function Update-WAUGUIFromConfig {
                 if ($shouldCheck) {
                     $updateInfo = Test-WAUGUIUpdate
                     if ($updateInfo.UpdateAvailable -and -not $updateInfo.Error) {
-                        $message = "Update available!`n`nCurrent version: $($updateInfo.CurrentVersion)`nLatest version: $($updateInfo.LatestVersion)`nRelease notes:`n$($updateInfo.ReleaseNotes -split "`n" | Where-Object { $_ -match '^(?:\*|-)[^*-]' })`n`nDo you want to download the update?"
+                        $message = "Update available!`n`nCurrent version: $($updateInfo.CurrentVersion)`nLatest version: $($updateInfo.LatestVersion)`nRelease notes:`n$(
+                            $updateInfo.ReleaseNotes -split "`n" | Where-Object { 
+                                $_ -match '^(?:\*|-)[^*-]' -or $_ -match '^\s+(?:\*|-)[^*-]' 
+                            }
+                        )`n`nDo you want to download the update?"
                         $result = [System.Windows.MessageBox]::Show($message, "Update Available", "OkCancel", "Question")
                         if ($result -eq 'Ok') {
                             Start-WAUGUIUpdate -updateInfo $updateInfo
